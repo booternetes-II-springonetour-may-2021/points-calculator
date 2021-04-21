af00e4cef62d3314ada9f5a0abfb2a2ea0d540a4
David Turanski <dturanski@vmware.com>
2021-04-21 14:48:54 -0400

    Fix deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,7 +6,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
- [[ $(helm list | tail -n +2 | grep spring-cloud-dataflow | awk '{print $8}') != "deployed" ]]; then
+if [[ $(helm list | tail -n +2 | grep spring-cloud-dataflow | awk '{print $8}') != "deployed" ]]; then
   echo "installing Spring Cloud Dataflow..."
   helm repo add bitnami https://charts.bitnami.com/bitnami
   helm install my-release bitnami/spring-cloud-dataflow
