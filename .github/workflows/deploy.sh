aa70b50bcd339a20d19857dbf5b8fa3e1ead1190
David Turanski <dturanski@vmware.com>
2021-04-21 16:03:15 -0400

    Still debugging...

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -12,6 +12,7 @@ if [[ $(helm list | tail -n +2 | grep spring-cloud-dataflow | awk '{print $8}') 
   helm repo add bitnami https://charts.bitnami.com/bitnami
   helm install --set server.service.type=LoadBalancer my-release bitnami/spring-cloud-dataflow
 else
+  kubectl get endpoints my-release-spring-cloud-dataflow-server
   kubectl get service my-release-spring-cloud-dataflow-server -o yaml
   export SERVICE_PORT=$(kubectl get --namespace default -o jsonpath="{..spec.ports[0].port}" services my-release-spring-cloud-dataflow-server)
   export SERVICE_IP=$(kubectl get svc --namespace default my-release-spring-cloud-dataflow-server -o jsonpath="{..status.loadBalancer.ingress[0].ip}")
