464203091589026ca371e55bdabe2896a8b05b3e
David Turanski <dturanski@vmware.com>
2021-04-21 14:56:10 -0400

    Add build and push app

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
-
+set -e
+set -o pipefail
 echo "installing Helm.."
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
@@ -12,4 +13,24 @@ if [[ $(helm list | tail -n +2 | grep spring-cloud-dataflow | awk '{print $8}') 
   helm install my-release bitnami/spring-cloud-dataflow
 fi
 
+echo "building and pushing app image ..."
 
+export PROJECT_ID=${GCLOUD_PROJECT:-pgtm-jlong}
+export ROOT_DIR=$(cd $(dirname $0) && pwd)
+
+cd $ROOT_DIR
+cd ../..
+pwd
+
+export APP_NAME=points-calculator-sink
+export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
+#docker rmi $(docker images -a -q)
+mvn clean package spring-boot:build-image
+
+image_id=$(docker images -q $APP_NAME)
+
+echo "tagging ${GCR_IMAGE_NAME}"
+docker tag "${image_id}" $GCR_IMAGE_NAME
+
+echo "pushing ${image_id} to $GCR_IMAGE_NAME "
+docker push $GCR_IMAGE_NAME
