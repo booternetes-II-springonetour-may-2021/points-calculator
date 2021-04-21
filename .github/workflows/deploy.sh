49ea00996714278ee96fc2f23574f34931564f6f
David Turanski <dturanski@vmware.com>
2021-04-21 15:23:27 -0400

    Debugging...

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash 
 set -e
 set -o pipefail
-echo "installing Helm.."
+echo "installing Helm..."
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
+kubectl config current-context
+
+helm uninstall my-release-spring-cloud-dataflow-server
+
 if [[ $(helm list | tail -n +2 | grep spring-cloud-dataflow | awk '{print $8}') != "deployed" ]]; then
   echo "installing Spring Cloud Dataflow..."
   helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm install my-release bitnami/spring-cloud-dataflow
+  helm install --set server.service.type=LoadBalancer my-release bitnami/spring-cloud-dataflow
 else
-  echo $(kubectl get services --namespace default)
-  echo $(kubectl get services --namespace default my-release-spring-cloud-dataflow -o yaml)
+  echo $(kubectl get services --namespace default my-release-spring-cloud-dataflow-server -o yaml)
 fi
 
 exit 0
